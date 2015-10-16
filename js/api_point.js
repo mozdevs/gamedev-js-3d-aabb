223b5bbabeb1ea706bdca22ffccdc86ad72f624e
Belén Albeza <belen.albeza@gmail.com>
2015-10-16 13:08:40 +0200

    add point vs box (api) demo

---
+++ js/api_point.js
@@ -0,0 +1,63 @@
+Game.init = function () {
+    this.debug = false;
+
+    var dotGeo = new THREE.SphereGeometry(0.05);
+
+    this.knot = new THREE.Mesh(
+        new THREE.TorusKnotGeometry(0.5, 0.1), this.materials.solid);
+    this.knot.position.x = -3;
+    this.knot.position.z = 1;
+    this.knot.position.y = 2;
+    this.knotBBox = new THREE.BoundingBoxHelper(this.knot, 0x00ff00);
+    this.knotBBox.update();
+    this.knotBBox.visible = false;
+
+    this.sphere = new THREE.Mesh(
+        new THREE.SphereGeometry(1), this.materials.solid);
+    this.sphere.position.x = 2;
+    this.sphere.position.y = 2;
+    this.sphereBBox = new THREE.BoundingBoxHelper(this.sphere, 0x00ff00);
+    this.sphereBBox.update();
+    this.sphereBBox.visible = false;
+
+    this.point = new THREE.Mesh(dotGeo, this.materials.dot);
+    this.point.position.x = 0.5;
+    this.point.position.z = 2;
+    this.point.position.y = 1;
+    this.pointShadow = Utils.createShadow(this.point, this.materials.shadow);
+
+    this.scene.add(this.point);
+    this.scene.add(this.knot);
+    this.scene.add(this.knotBBox);
+    this.scene.add(this.sphere);
+    this.scene.add(this.sphereBBox);
+    // add fake shadows
+    this.scene.add(Utils.createShadow(this.sphere, this.materials.shadow));
+    this.scene.add(Utils.createShadow(this.knot, this.materials.shadow));
+    this.scene.add(this.pointShadow);
+
+    this.controls = new THREE.TransformControls(
+        this.camera, this.renderer.domElement);
+    this.controls.space = 'world';
+    this.controls.attach(this.point);
+    this.scene.add(this.controls);
+};
+
+Game.update = function (delta) {
+    this.controls.update();
+    Utils.updateShadow(this.pointShadow, this.point);
+
+    this.sphere.material = this.sphereBBox.box.containsPoint(this.point.position)
+        ? this.materials.colliding
+        : this.materials.solid;
+
+    this.knot.material = this.knotBBox.box.containsPoint(this.point.position)
+        ? this.materials.colliding
+        : this.materials.solid;
+};
+
+Game.toggleDebug = function () {
+    this.debug = !this.debug;
+    this.knotBBox.visible = !!this.debug;
+    this.sphereBBox.visible = !!this.debug;
+};kk
---
+++ js/common.js
@@ -0,0 +1,120 @@
+//
+// Game singleton
+//
+
+var Game = {};
+
+Game.run = function () {
+    var WIDTH = 512;
+    var HEIGHT = 512;
+
+    this._previousElapsed = 0;
+
+    // setup a WebGL renderer within an existing canvas
+    var canvas = document.getElementById('demo');
+    this.renderer = new THREE.WebGLRenderer({canvas: canvas});
+    canvas.width = WIDTH;
+    canvas.height = HEIGHT;
+    this.renderer.setViewport(0, 0, WIDTH, HEIGHT);
+
+    // create the scene
+    this.scene = new THREE.Scene();
+
+    // create an isometric camera
+    this.camera = new THREE.OrthographicCamera(
+        -5, 5, 5, -5, -1, 100);
+    this.camera.position.z = 5;
+    this.camera.position.y = 5;
+    this.camera.position.x = 5;
+    this.camera.lookAt(this.scene.position); // point at origin
+
+    // create ground and axis / grid helpers
+    var ground = new THREE.Mesh(new THREE.PlaneGeometry(10, 10),
+        new THREE.MeshBasicMaterial({color: 0xcccccc}));
+    ground.rotation.x = -Math.PI / 2;
+    ground.position.y = -0.01; // to avoid z-fighting with axis and shadows
+    this.scene.add(ground);
+    this.scene.add((new THREE.AxisHelper(8)));
+
+
+    document.addEventListener('keyup', function (event) {
+        if (event.keyCode === 27) { // listen for Esc Key
+            event.preventDefault();
+            this.toggleDebug();
+        }
+    }.bind(this));
+
+    // start up game
+    this.init();
+    window.requestAnimationFrame(this.tick);
+};
+
+Game.tick = function (elapsed) {
+    window.requestAnimationFrame(this.tick);
+
+    // compute delta time in seconds -- also cap it
+    var delta = (elapsed - this._previousElapsed) / 1000.0;
+    delta = Math.min(delta, 0.25); // maximum delta of 250 ms
+    this._previousElapsed = elapsed;
+
+    this.update(delta);
+    this.renderer.render(this.scene, this.camera);
+}.bind(Game);
+
+// collection of materials used in the demos
+Game.materials = {
+    shadow: new THREE.MeshBasicMaterial({
+        color: 0x000000,
+        transparent: true,
+        opacity: 0.5
+    }),
+    solid: new THREE.MeshNormalMaterial({}),
+    colliding: new THREE.MeshBasicMaterial({
+        color: 0xff0000,
+        transparent: true,
+        opacity: 0.5
+    }),
+    dot: new THREE.MeshBasicMaterial({
+        color: 0x0000ff
+    })
+};
+
+// override these methods to create the demo
+Game.init = function () {};
+Game.update = function (delta) {};
+Game.toggleDebug = function () {};
+
+//
+// Utils
+//
+
+var Utils =  {};
+
+Utils.createShadow = function (mesh, material) {
+    var params = mesh.geometry.parameters;
+    var geo = mesh.geometry.type === 'BoxGeometry'
+        ? new THREE.PlaneGeometry(params.width, params.height)
+        : new THREE.CircleGeometry(params.radius, 24);
+
+    var shadow = new THREE.Mesh(geo, material);
+    shadow.rotation.x = -Math.PI / 2;
+    shadow.position.x = mesh.position.x;
+    shadow.position.z = mesh.position.z;
+
+    return shadow;
+};
+
+Utils.updateShadow = function (shadow, target) {
+    shadow.position.x = target.position.x;
+    shadow.position.z = target.position.z;
+    shadow.visible = target.position.y >= 0;
+};
+
+
+//
+// main
+//
+
+window.onload = function () {
+    Game.run();
+};
